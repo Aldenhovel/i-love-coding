2c360d44b2a7133c6452cdc26831d015a96e7865
Aldenhovel Liang <1736724492@qq.com>
2022-11-16 23:05:25 +0800

    update cs files

---
+++ cs/A-star algorithm/A_star.cs
@@ -0,0 +1,435 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace AI {
+    class Node {
+
+        public string node_name;
+        public int comsume;
+        public int distance;
+        public string parents_name;
+        public int H;
+
+        public Node() {
+
+        }
+
+        public Node(string name, int H) {
+            this.node_name = name;
+            this.comsume = 0;
+            this.distance = 0;
+            this.parents_name = " ";
+            this.H = H;
+        }
+
+
+        public Node[] GetNeighbourNode(Node node, Edge[] edge, Node[] all_nodes) {
+            //输入：node当前节点，edge所有边的集，all_node所有节点的集
+            //输出：所有与node相邻的节点的数组
+
+            Node[] neighbour_node = new Node[100];
+            for(int i = 0; i < 100; i++) {
+                neighbour_node[i] = new Node();
+            }
+            int count = 0;
+
+            foreach (Edge e in edge) {
+                if (e.node1 == node.node_name) {
+                    neighbour_node[count] = GetNode(e.node2, all_nodes);
+                    count++;
+                } else if (e.node2 == node.node_name) {
+                    neighbour_node[count] = GetNode(e.node1, all_nodes);
+                    count++;
+                }
+            }
+            return neighbour_node;
+        }
+
+        public Node FindMinConsumeNode(OPEN open) {
+            //输入：当前OPEN表
+            //输出：当前OPEN表中consume值最小的节点
+
+            int consume_temp = int.MaxValue;
+            Node node_min = new Node();
+
+            foreach (Node node in open.list) {
+                if (node.comsume < consume_temp) {
+                    node_min = node;
+                    consume_temp = node.comsume;
+                }
+            }
+            return node_min;
+        }
+
+        public Node GetNode(string node_name, OPEN open) {
+            //输入：要查找的节点名字，当前OPEN表
+            //输出：名字对应的节点实体
+
+            foreach (Node n in open.list) {
+                if (n.node_name == node_name) {
+                    return n;
+                }
+            }
+            return null;
+        }
+        public Node GetNode(string node_name, CLOSE close) {
+            //输入：要查找的节点名字，当前CLOSE表
+            //输出：名字对应的节点实体
+
+            foreach (Node n in close.list) {
+                if (n.node_name == node_name) {
+                    return n;
+                }
+            }
+            return null;
+        }
+        public Node GetNode(string node_name, Node[] nodes) {
+            //输入：要查找的节点名字，全体节点成员
+            //输出：名字对应的节点实体
+
+            foreach (Node n in nodes) {
+                if (n.node_name == node_name) {
+                    return n;
+                }
+            }
+            return null;
+        }
+
+    }
+
+    class Edge {
+
+        public string node1;
+        public string node2;
+        public int cost;
+
+        public Edge() {
+
+        }
+        public Edge(string node1,string node2,int cost) {
+            this.node1 = node1;
+            this.node2 = node2;
+            this.cost = cost;
+        }
+
+        public int GetEdgeCost(string node1,string node2,Edge[] edges) {
+            int cost = -1;
+            foreach (Edge e in edges) {
+                if (( ( e.node1 == node1 ) && ( e.node2 == node2 ) ) || ( ( e.node2 == node1 ) && ( e.node1 == node2 ) )) {
+                    cost = e.cost;
+                }
+            }
+            return cost;
+        }
+
+    }
+
+    class OPEN {
+
+        public Node[] list;
+
+        public OPEN() {
+            this.list = new Node[100];
+            for(int i = 0; i < 100; i++) {
+                this.list[i] = new Node();
+                this.list[i].comsume = int.MaxValue;
+            }
+            foreach (Node n in list) {
+                n.distance = int.MaxValue;
+            }
+        }
+
+        public OPEN Remove(string node_name,OPEN open) {
+            //输入：要删除的节点名，当前OPEN表
+            //输出：删除节点后整理好的OPEN表
+
+            int node_position = 0;
+            foreach(Node n in open.list) {
+                if(n.node_name == node_name) {
+                    break;
+                } 
+                else {
+                    node_position++;
+                }
+            }
+
+            //不存在该节点则退出返回原表
+            if (( node_position == 0 ) && ( open.list[0].node_name != node_name )) {
+                return open;
+            }
+
+            //整理队列，position之后的节点前移
+            int position_plus_one = node_position + 1;
+            while (position_plus_one < 99) {
+                open.list[node_position] = open.list[position_plus_one];
+                position_plus_one++;
+                node_position++;
+            }
+            open.list[99].distance = int.MaxValue;
+            return open;
+        }
+
+        public  OPEN Append(Node node,OPEN open) {
+            //输入：节点名字，当前OPEN表
+            //输出：新增节点后的OPEN表
+
+            //列表已满无法增加，返回NULL
+            if (open.list[99].distance < int.MaxValue) {
+                return null;
+            }
+
+            //找到空闲的第一个位置
+            int free_position = 0;
+            foreach(Node n in open.list) {
+                if (n.distance == int.MaxValue) {
+                    break;
+                } 
+                else {
+                    free_position++;
+                }
+            }
+
+            //插入节点并返回OPEN表
+            open.list[free_position] = node;
+            return open;
+        }
+
+        public bool IsEmpty(OPEN open) {
+            //判断OPEN表是否为空
+
+            if (open.list[0].distance == int.MaxValue) {
+                return true;
+            } 
+            else {
+                return false;
+            }
+        }
+
+        public bool IsExist(string node_name,OPEN open) {
+            //查找OPEN表中是否存在节点node_name
+
+            foreach(Node n in open.list) {
+                if(n.node_name == node_name) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+        
+    }//class OPEN
+
+    class CLOSE {
+
+        public Node[] list;
+
+        public CLOSE() {
+            this.list = new Node[100];
+            for(int i = 0; i < 100; i++) {
+                this.list[i] = new Node();
+                this.list[i].comsume = int.MaxValue;
+            }
+            foreach(Node n in list) {
+                n.distance = int.MaxValue;
+            }
+        }
+
+        public CLOSE Remove(string node_name,CLOSE close) {
+            //输入：要删除的节点名，当前OPEN表
+            //输出：删除节点后整理好的OPEN表
+
+            int node_position = 0;
+            foreach (Node n in close.list) {
+                if (n.node_name == node_name) {
+                    break;
+                } else {
+                    node_position++;
+                }
+            }
+
+            //不存在该节点则退出返回原表
+            if (( node_position == 0 ) && ( close.list[0].node_name != node_name )) {
+                return close;
+            }
+
+            //整理队列，position之后的节点前移
+            int position_plus_one = node_position + 1;
+            while (position_plus_one < 99) {
+                close.list[node_position] = close.list[position_plus_one];
+                position_plus_one++;
+                node_position++;
+            }
+            close.list[99].distance = int.MaxValue;
+            return close;
+        }
+
+        public CLOSE Append(Node node,CLOSE close) {
+            //输入：节点名字，当前OPEN表
+            //输出：新增节点后的OPEN表
+
+            //列表已满无法增加，返回NULL
+            if (close.list[99].distance < int.MaxValue) {
+                return null;
+            }
+
+            //找到空闲的第一个位置
+            int free_position = 0;
+            foreach (Node n in close.list) {
+                if (n.distance == int.MaxValue) {
+                    break;
+                } else {
+                    free_position++;
+                }
+            }
+
+            //插入节点并返回OPEN表
+            close.list[free_position] = node;
+            return close;
+        }
+
+        public bool IsExist(string node_name,CLOSE close) {
+            //查找close表中是否存在节点node_name
+
+            foreach (Node n in close.list) {
+                if(n.node_name == node_name) {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        public static void Show(CLOSE close) {
+            //打印出CLOSE表中的所有路径
+
+            foreach(Node n in close.list) {
+                if (n.comsume != int.MaxValue) {
+                    Console.WriteLine(n.parents_name + ">>" + n.node_name 
+                        + "        " + n.node_name + "的comsume值(fn)：" +n.comsume.ToString() 
+                        + "\t累计distance值(实际路径耗散)：" + n.distance.ToString());
+                }            
+            }
+        }
+
+    }//class CLOSE
+
+    class A_star {
+
+        public A_star() {
+
+        }
+
+        public static CLOSE Run(Node[] nodes, Edge[] edges, string start_name, string end_name,bool show_process=false) {
+            //A*算法
+            //输入：点集nodes,边集edges,起始点名start_name,终结点名end_name
+            //输出：最终CLOSE表
+
+            //初始化
+            Node NodeFunction = new Node();
+            Edge EdgeFunction = new Edge();
+            OPEN OPENFunction = new OPEN();
+            CLOSE CLOSEFunction = new CLOSE();
+
+            OPEN open = new OPEN();
+            CLOSE close = new CLOSE();
+
+            Node now_node = NodeFunction.GetNode(start_name, nodes);
+            now_node.comsume = now_node.H;//Console.WriteLine(now_node.comsume.ToString());
+            now_node.distance = 0;
+            now_node.parents_name = now_node.node_name;
+
+            //将起始点放到OPEN表中，此时OPEN表为空
+            close = CLOSEFunction.Append(now_node, close);
+
+            //设置循环计数器
+            int loop_count = 0;
+
+            //主循环：当OPEN中非空时，或第一次循环OPEN表空而CLOSE表只有一个节点时，要扩展
+            while ((OPENFunction.IsEmpty(open) != true) || (close.list[1].comsume == int.MaxValue)) {
+
+                loop_count++;
+                Node[] childrennode = NodeFunction.GetNeighbourNode(now_node, edges, nodes);
+
+                foreach(Node n in childrennode) {
+
+                    //计算每个child_node的consume值
+                    int child_consume = now_node.distance 
+                        + n.H 
+                        + EdgeFunction.GetEdgeCost(now_node.node_name, n.node_name,edges);
+
+                    //分类处理每个child_node
+                    //不在两表中，则加入OPEN表
+                    if ((OPENFunction.IsExist(n.node_name, open) == false) 
+                        && (CLOSEFunction.IsExist(n.node_name, close) == false)) {
+
+                        n.comsume = child_consume;
+                        n.parents_name = now_node.node_name;//需要指定parents?
+                        open = OPENFunction.Append(n, open);
+                    }
+
+                    //在OPEN表中，则对比原consume值，新consume值更小则更新其consume,parents
+                    else if (OPENFunction.IsExist(n.node_name, open) == true) {
+
+                        if (child_consume < n.comsume) {
+                            n.comsume = child_consume;
+                            n.parents_name = now_node.node_name;
+                        }
+                    }
+
+                    //在CLOSE表中，则对比原consume值，新consume值更小则更新其consume,parents,并将该结点从CLOSE移到OPEN
+                    else if (CLOSEFunction.IsExist(n.node_name, close) == true) {
+
+                        if(child_consume < n.comsume) {
+                            n.comsume = child_consume;
+                            n.parents_name = now_node.node_name;
+                            n.distance = int.MaxValue;
+                            close = CLOSEFunction.Remove(n.node_name,close);
+                            open = OPENFunction.Append(n, open);
+                        }
+                    }
+
+                }//end foreach
+
+                //确定要扩展的节点，从OPEN表中找出consume值最小的
+                Node expand_node = NodeFunction.FindMinConsumeNode(open);
+
+                //记录扩展点的耗散distance = 父节点的distance + 两节点边的耗散值(g(n+1)=g(n)+e)
+                expand_node.distance = NodeFunction.GetNode(expand_node.parents_name, nodes).distance
+                                                        + EdgeFunction.GetEdgeCost(expand_node.node_name, expand_node.parents_name,edges);
+
+                //将扩展节点作为主节点，将他由OPEN表移至CLOSE表
+                now_node = expand_node;
+                open = OPENFunction.Remove(now_node.node_name, open);
+                close = CLOSEFunction.Append(now_node, close);
+
+                if (show_process == true) {
+                    Console.WriteLine("CLOSE表第" + loop_count.ToString() + "次循环");
+                    CLOSE.Show(close);
+                    Console.WriteLine("\n\n\n\n");
+                }
+            }
+
+            //返回目标CLOSE表
+            return close;
+        }
+
+        public static void Show(CLOSE close) {
+            Console.WriteLine("最终路径图（a>>b表示a指向b的路径）：");
+            CLOSE.Show(close);
+        }
+
+
+
+    }
+   
+
+
+
+
+}//namespace
+
+
---
+++ cs/A-star algorithm/Program.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AI {
+    class Program {
+        static void Main(string[] args) {
+
+
+            //测试集1
+            {
+                Node[] nodes1 = new Node[5];
+
+                Node s = new Node("s", 10);
+                Node a = new Node("a", 1);
+                Node b = new Node("b", 5);
+                Node c = new Node("c", 8);
+                Node g = new Node("g", 0);
+
+                nodes1[0] = s;
+                nodes1[1] = a;
+                nodes1[2] = b;
+                nodes1[3] = c;
+                nodes1[4] = g;
+
+                Edge[] edges1 = new Edge[6];
+
+                Edge sa = new Edge("s", "a", 6);
+                Edge sb = new Edge("s", "b", 3);
+                Edge sc = new Edge("s", "c", 1);
+                Edge bc = new Edge("b", "c", 1);
+                Edge ab = new Edge("a", "b", 1);
+                Edge ag = new Edge("a", "g", 8);
+
+                edges1[0] = sa;
+                edges1[1] = sb;
+                edges1[2] = sc;
+                edges1[3] = bc;
+                edges1[4] = ab;
+                edges1[5] = ag;
+
+
+
+                 CLOSE finally_close = A_star.Run(nodes1, edges1, "s", "g",true);
+                A_star.Show(finally_close);
+            }
+
+
+            //测试集2
+            {
+                Node[] nodes2 = new Node[8];
+
+                Node A = new Node("A", 15);
+                Node B = new Node("B", 14);
+                Node C = new Node("C", 10);
+                Node D = new Node("D", 2);
+                Node E = new Node("E", 0);
+                Node F = new Node("F", 5);
+                Node G = new Node("G", 9);
+                Node H = new Node("H", 11);
+
+                nodes2[0] = A;
+                nodes2[1] = B;
+                nodes2[2] = C;
+                nodes2[3] = D;
+                nodes2[4] = E;
+                nodes2[5] = F;
+                nodes2[6] = G;
+                nodes2[7] = H;
+
+                Edge[] edges2 = new Edge[11];
+
+                edges2[0] = new Edge("A", "B", 3);
+                edges2[1] = new Edge("A", "H", 4);
+                edges2[2] = new Edge("B", "H", 5);
+                edges2[3] = new Edge("B", "C", 4);
+                edges2[4] = new Edge("G", "H", 2);
+                edges2[5] = new Edge("C", "G", 3);
+                edges2[6] = new Edge("C", "D", 8);
+                edges2[7] = new Edge("G", "F", 4);
+                edges2[8] = new Edge("D", "G", 8);
+                edges2[9] = new Edge("D", "F", 3);
+                edges2[10] = new Edge("D", "E", 2);
+
+
+                //CLOSE finally_close = A_star.Run(nodes2, edges2, "A", "E", true);
+                //A_star.Show(finally_close);
+            }
+
+
+
+            //测试集3
+            {
+                Node[] nodes3 = new Node[6];
+
+                nodes3[0] = new Node("s", 20);
+                nodes3[1] = new Node("A", 1);
+                nodes3[2] = new Node("B", 4);
+                nodes3[3] = new Node("C", 8);
+                nodes3[4] = new Node("D", 14);
+                nodes3[5] = new Node("t", 18);
+
+                Edge[] edges3 = new Edge[11];
+
+                edges3[0] = new Edge("s", "A", 11);
+                edges3[1] = new Edge("s", "B", 9);
+                edges3[2] = new Edge("s", "C", 6);
+                edges3[3] = new Edge("s", "D", 1);
+                edges3[4] = new Edge("A", "B", 1);
+                edges3[5] = new Edge("B", "D", 4);
+                edges3[6] = new Edge("A", "D", 6);
+                edges3[7] = new Edge("A", "C", 3);
+                edges3[8] = new Edge("B", "C", 1);
+                edges3[9] = new Edge("C", "D", 1);
+                edges3[10] = new Edge("A", "t", 18);
+
+                //CLOSE finally_close = A_star.Run(nodes3, edges3, "s", "t", true);
+                //A_star.Show(finally_close);
+
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+
+  
+
+}
