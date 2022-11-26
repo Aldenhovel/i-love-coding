e565eb8bd790e3e455abbb4b7cf6b63cf9b2c4d4
aldenhovel <1736724492@qq.com>
2022-11-26 20:36:34 +0800

    update cpp files

---
+++ cpp/RTTI/RTTI.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+class Grand {
+private:
+	int hold;
+public:
+	Grand(int h = 0) : hold(h) {};
+	virtual void Speak() const { cout << "I M Grand" << endl; }
+	virtual int Value() const { return hold; }
+};
+
+class Superb : public Grand {
+public:
+	Superb(int h = 0) : Grand(h) {};
+	void Speak() const { cout << "I M Superb" << endl; }
+	virtual void Say() const { cout << "I hold superb value of " << Value() << endl; }
+};
+
+class Magnificent : public Superb {
+private:
+	char ch;
+public:
+	Magnificent(int h = 0, char c = 'A') : Superb(h), ch(c) {};
+	void Speak() const { cout << "I M Magnificent" << endl; }
+	void Say() const { cout << "I hold the char " << ch << " and value " << Value() << endl; }
+};
+
+Grand* GetOne() {
+	// 随机生成 Grand Superb Magnificent 三个类中的一个
+	Grand *p;
+	switch(std::rand() % 3) {
+		case 0:
+			p = new Grand(std::rand() % 100);
+			break;
+		case 1:
+			p = new Superb(std::rand() % 100);
+			break;
+		case 2:
+			p = new Magnificent(std::rand() % 100, 'A' + std::rand() % 26);
+	}
+	return p;
+}
+
+int main() {
+	std::srand(std::time(0));
+	Grand *pg;
+	Superb *ps;
+	for (int i = 0; i < 5; i++) {
+		pg = GetOne();
+		pg->Speak();
+
+		if  (ps = dynamic_cast<Superb*>(pg)) ps->Say();
+	}
+	return 0;
+}
